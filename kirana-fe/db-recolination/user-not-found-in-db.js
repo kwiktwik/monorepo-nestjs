f5cf7e46e4cdaeb0c5fb3262536f2f298fc1536e
rajat <rajatdhoot123@gmail.com>
2026-03-10 22:22:46 +0530

    User Sync script added

---
+++ kirana-fe/db-recolination/user-not-found-in-db.js
@@ -0,0 +1,237 @@
+[
+    {
+        "razorpayOrderId": "order_SP3hYxx4OMqDwq",
+        "status": "error",
+        "message": "User not found for 917033572890@kiranaapps.local"
+    },
+    {
+        "razorpayOrderId": "order_SOxa36Um2842tS",
+        "status": "error",
+        "message": "User not found for 918872509105@kiranaapps.local"
+    },
+    {
+        "razorpayOrderId": "order_SPCh77t4Yvd0ZX",
+        "status": "error",
+        "message": "User not found for 919284572272@kiranaapps.local"
+    },
+    {
+        "razorpayOrderId": "order_SOyIGba3AbBJEu",
+        "status": "error",
+        "message": "User not found for 919106251607@alertpay.local"
+    },
+    {
+        "razorpayOrderId": "order_SP2sj9JvnCX0c1",
+        "status": "error",
+        "message": "User not found for 919341175648@alertpay.local"
+    },
+    {
+        "razorpayOrderId": "order_SP09XJVmE5iKPL",
+        "status": "error",
+        "message": "User not found for 916204659264@kiranaapps.local"
+    },
+    {
+        "razorpayOrderId": "order_SP9HnalnvlWWqG",
+        "status": "error",
+        "message": "User not found for 919250221048@kiranaapps.local"
+    },
+    {
+        "razorpayOrderId": "order_SP8chntMVe60NI",
+        "status": "error",
+        "message": "User not found for 917893522487@alertpay.local"
+    },
+    {
+        "razorpayOrderId": "order_SP4nJOm65TNtWg",
+        "status": "error",
+        "message": "User not found for 919919031515@alertpay.local"
+    },
+    {
+        "razorpayOrderId": "order_SP5rypLh7gOekX",
+        "status": "error",
+        "message": "User not found for 916356069083@alertpay.local"
+    },
+    {
+        "razorpayOrderId": "order_SPDgOTXUpcoZzs",
+        "status": "error",
+        "message": "User not found for 919499647748@kiranaapps.local"
+    },
+    {
+        "razorpayOrderId": "order_SPD9MoV2dh3xjm",
+        "status": "error",
+        "message": "User not found for 917738332596@alertpay.local"
+    },
+    {
+        "razorpayOrderId": "order_SP5QmC4mejESGS",
+        "status": "error",
+        "message": "User not found for 919845228856@kiranaapps.local"
+    },
+    {
+        "razorpayOrderId": "order_SP0tJiaOMbfpTB",
+        "status": "error",
+        "message": "User not found for 919305180596@kiranaapps.local"
+    },
+    {
+        "razorpayOrderId": "order_SOzQXjDNu23NbX",
+        "status": "error",
+        "message": "User not found for 919157682522@kiranaapps.local"
+    },
+    {
+        "razorpayOrderId": "order_SP6V8SvRX5hMos",
+        "status": "error",
+        "message": "User not found for 919165106804@kiranaapps.local"
+    },
+    {
+        "razorpayOrderId": "order_SP5T4tuhDdyVNy",
+        "status": "error",
+        "message": "User not found for 919817790399@alertpay.local"
+    },
+    {
+        "razorpayOrderId": "order_SP708gYneJ06IV",
+        "status": "error",
+        "message": "User not found for 916200488928@kiranaapps.local"
+    },
+    {
+        "razorpayOrderId": "order_SP873vpZFHAfYc",
+        "status": "error",
+        "message": "User not found for 919244061575@kiranaapps.local"
+    },
+    {
+        "razorpayOrderId": "order_SOq8gkIZtt6TH2",
+        "status": "error",
+        "message": "User not found for 918586830786@alertpay.local"
+    },
+    {
+        "razorpayOrderId": "order_SP1s2eccoE9nr2",
+        "status": "error",
+        "message": "User not found for 918521832040@alertpay.local"
+    },
+    {
+        "razorpayOrderId": "order_SOzShSAHCFYK75",
+        "status": "error",
+        "message": "User not found for 919636680102@alertpay.local"
+    },
+    {
+        "razorpayOrderId": "order_SP6kxu3Z24gGWE",
+        "status": "error",
+        "message": "User not found for 919694565156@alertpay.local"
+    },
+    {
+        "razorpayOrderId": "order_SOzDeDBs0IFqzN",
+        "status": "error",
+        "message": "User not found for 919906156648@alertpay.local"
+    },
+    {
+        "razorpayOrderId": "order_SP3FpCTlQEFfVI",
+        "status": "error",
+        "message": "User not found for 918517941015@alertpay.local"
+    },
+    {
+        "razorpayOrderId": "order_SP6ADRQqiVpCt1",
+        "status": "error",
+        "message": "User not found for 916200488928@kiranaapps.local"
+    },
+    {
+        "razorpayOrderId": "order_SP2NKxtaIbNjwO",
+        "status": "error",
+        "message": "User not found for 918273989371@kiranaapps.local"
+    },
+    {
+        "razorpayOrderId": "order_SP7MECD5qmknuD",
+        "status": "error",
+        "message": "User not found for 919210097499@kiranaapps.local"
+    },
+    {
+        "razorpayOrderId": "order_SPByBRhRcy38tn",
+        "status": "error",
+        "message": "User not found for 919440497155@alertpay.local"
+    },
+    {
+        "razorpayOrderId": "order_SPCBKc6GisywAz",
+        "status": "error",
+        "message": "User not found for 919835188741@kiranaapps.local"
+    },
+    {
+        "razorpayOrderId": "order_SP5M37jHdHYuJB",
+        "status": "error",
+        "message": "User not found for 918305430730@alertpay.local"
+    },
+    {
+        "razorpayOrderId": "order_SP26bEcfS734WU",
+        "status": "error",
+        "message": "User not found for 919030422918@alertpay.local"
+    },
+    {
+        "razorpayOrderId": "order_SOzMfrzyKkehEr",
+        "status": "error",
+        "message": "User not found for 917876184923@alertpay.local"
+    },
+    {
+        "razorpayOrderId": "order_SPDUjGRiGjAdoR",
+        "status": "error",
+        "message": "User not found for 917489316939@kiranaapps.local"
+    },
+    {
+        "razorpayOrderId": "order_SP5Ndu1hV8Dmox",
+        "status": "error",
+        "message": "User not found for 918305430730@alertpay.local"
+    },
+    {
+        "razorpayOrderId": "order_SP40dr0nn7g4B4",
+        "status": "error",
+        "message": "User not found for 919109231155@alertpay.local"
+    },
+    {
+        "razorpayOrderId": "order_SP2RgsK7wnqB9d",
+        "status": "error",
+        "message": "User not found for 917462926362@kiranaapps.local"
+    },
+    {
+        "razorpayOrderId": "order_SP43N4dWK6r109",
+        "status": "error",
+        "message": "User not found for 919920464516@kiranaapps.local"
+    },
+    {
+        "razorpayOrderId": "order_SP27Y2jUhvU6ZE",
+        "status": "error",
+        "message": "User not found for 919267981188@kiranaapps.local"
+    },
+    {
+        "razorpayOrderId": "order_SP4ubooQFVdpRN",
+        "status": "error",
+        "message": "User not found for 919815284469@kiranaapps.local"
+    },
+    {
+        "razorpayOrderId": "order_SOzN4UwGlnQ4EB",
+        "status": "error",
+        "message": "User not found for 919555293409@kiranaapps.local"
+    },
+    {
+        "razorpayOrderId": "order_SOzG5yZ7CVCfWI",
+        "status": "error",
+        "message": "User not found for 918140343539@alertpay.local"
+    },
+    {
+        "razorpayOrderId": "order_SOuwzqbxondrIp",
+        "status": "error",
+        "message": "User not found for 917780826147@alertpay.local"
+    },
+    {
+        "razorpayOrderId": "order_SP5DADTz0I3Ztb",
+        "status": "error",
+        "message": "User not found for 919583716932@alertpay.local"
+    },
+    {
+        "razorpayOrderId": "order_SP0j9c762tBzIN",
+        "status": "error",
+        "message": "User not found for 916392657561@kiranaapps.local"
+    },
+    {
+        "razorpayOrderId": "order_SP1pjFCBgNuQmc",
+        "status": "error",
+        "message": "User not found for 916296741147@kiranaapps.local"
+    },
+    {
+        "razorpayOrderId": "order_SOzSJzdi43tlSH",
+        "status": "error",
+        "message": "User not found for 919555293409@kiranaapps.local"
+    }
+]
