86bbfcc2e9a16097a80b260980156f0592583d7b
rajat <rajatdhoot123@gmail.com>
2026-03-23 23:59:27 +0530

    Kirana BE added

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -22,7 +22,7 @@ module.exports = {
       listen_timeout: 10000,
       script: "dist/src/main.js",
       cwd: "/home/ubuntu/project/backend-monorepo/kwiktwik-kirana-be",
-      instances: 1,
+      instances: 2,
       exec_mode: "cluster",
       env_production: {
         PORT: 4010,
