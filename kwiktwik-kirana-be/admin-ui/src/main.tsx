32fc93ee8f21dca79769a533519fd28b62194919
rajat <rajatdhoot123@gmail.com>
2026-04-05 12:28:52 +0530

    Added More Bundle

--- kwiktwik-kirana-be/admin-ui/src/main.tsx
+++ kwiktwik-kirana-be/admin-ui/src/main.tsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.tsx';
+import { AuthProvider } from './context/AuthContext';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     {/* Base router path required to match NestJS ServeStaticModule serveRoot */}
     <BrowserRouter basename="/admin">
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </StrictMode>
 );
