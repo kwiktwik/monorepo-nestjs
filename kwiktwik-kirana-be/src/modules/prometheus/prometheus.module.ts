2d37fd51abfbca9bcb2e7b7f61fb1455384af6c0
rajat <rajat@rajatmacbook.local>
2026-04-23 20:07:46 +0530

    API monitoring added

---
+++ kwiktwik-kirana-be/src/modules/prometheus/prometheus.module.ts
@@ -0,0 +1,40 @@
+import { Module, Global } from '@nestjs/common';
+import {
+  PrometheusModule as PrometheusModuleBase,
+} from '@willsoto/nestjs-prometheus';
+import { metricProviders } from './metrics.providers';
+import { HealthMetricsService } from './health-metrics.service';
+import { MetricsController } from './metrics.controller';
+import { PrometheusMetricsInterceptor } from '../../common/interceptors/prometheus-metrics.interceptor';
+
+/**
+ * Prometheus Module - Provides metrics collection and exposure capabilities
+ * 
+ * This module:
+ * - Registers the base Prometheus module for metrics endpoint
+ * - Provides custom metric providers (counters, gauges, histograms)
+ * - Exports the PrometheusMetricsInterceptor for use in other modules
+ * - Provides health metrics service
+ * 
+ * Import this module in any module that uses @UseInterceptors(PrometheusMetricsInterceptor)
+ */
+@Global()
+@Module({
+  imports: [
+    PrometheusModuleBase.register({
+      path: '/internal/metrics',
+    }),
+  ],
+  controllers: [MetricsController],
+  providers: [
+    HealthMetricsService,
+    PrometheusMetricsInterceptor,
+    ...metricProviders,
+  ],
+  exports: [
+    PrometheusMetricsInterceptor,
+    ...metricProviders,
+    HealthMetricsService,
+  ],
+})
+export class PrometheusModule {}
