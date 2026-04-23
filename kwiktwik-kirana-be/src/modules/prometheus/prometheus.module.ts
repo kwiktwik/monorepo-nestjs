c3390f22bd48a087c9ab1143fd2e756d4a0a2565
rajat <rajat@rajatmacbook.local>
2026-04-23 15:41:16 +0530

    Check Promo working

--- kwiktwik-kirana-be/src/modules/prometheus/prometheus.module.ts
+++ kwiktwik-kirana-be/src/modules/prometheus/prometheus.module.ts
@@ -1,7 +1,9 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { PrometheusModule as PrometheusModuleBase } from '@willsoto/nestjs-prometheus';
+import { collectDefaultMetrics } from 'prom-client';
 import { HealthMetricsService } from './health-metrics.service';
 import { metricProviders } from './metrics.providers';
+import { MetricsController } from './metrics.controller';
 
 /**
  * Prometheus metrics module for monitoring
@@ -11,11 +13,21 @@ import { metricProviders } from './metrics.providers';
  */
 @Module({
   imports: [
+    // Register the base module for metric providers (counter, gauge, etc.)
+    // We don't use its auto-controller - we use our own explicit controller
     PrometheusModuleBase.register({
-      path: '/metrics',
+      path: '/metrics-internal', // Different path to avoid conflicts
     }),
   ],
+  controllers: [MetricsController],
   providers: [HealthMetricsService, ...metricProviders],
   exports: [HealthMetricsService, ...metricProviders],
 })
-export class PrometheusModule {}
+export class PrometheusModule implements OnModuleInit {
+  onModuleInit() {
+    // Collect default Node.js metrics (event loop, heap, gc, etc.)
+    collectDefaultMetrics({
+      eventLoopMonitoringPrecision: 10,
+    });
+  }
+}
