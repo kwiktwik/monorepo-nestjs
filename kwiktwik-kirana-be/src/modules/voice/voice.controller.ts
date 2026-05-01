ed96ff01dc94c54cd2e517821a7b6e89d7925c51
rajat <rajat@rajatmacbook.local>
2026-05-01 12:59:56 +0530

    remove from env

--- kwiktwik-kirana-be/src/modules/voice/voice.controller.ts
+++ kwiktwik-kirana-be/src/modules/voice/voice.controller.ts
@@ -242,7 +242,7 @@ export class VoiceController {
     let provider: VoiceProvider;
     switch (config.provider) {
       case 'gemini':
-        provider = new GeminiVoiceProvider(config.apiKey, config.model);
+        provider = new GeminiVoiceProvider(config.apiKey);
         break;
       default:
         throw new ServiceUnavailableException(`Unknown provider: ${config.provider}`);
